02d840c0c6ec29f36dc96e52537796ed176dc512
Luca Abbati <luca.abbati@gmail.com>
2018-07-13 08:25:39 +0200

    Add models to represent the core entities, an action and a constraint on an action

---
+++ src/main/java/com/labbati/cando/model/Action.java
@@ -0,0 +1,53 @@
+package com.labbati.cando.model;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.BooleanSupplier;
+import java.util.function.Predicate;
+
+public class Action {
+
+    private String name;
+
+    private boolean allowed;
+
+    private Set<Constraint> constraints;
+
+    public Action(String name, boolean allowed, Set<Constraint> constraints) {
+        this.name = name;
+        this.constraints = constraints;
+        this.allowed = allowed;
+    }
+
+    public Action(String name, boolean allowed) {
+        this(name, allowed, new HashSet<>());
+    }
+
+    public static Action allow(String name) {
+        return new Action(name, true);
+    }
+
+    public static Action allow(String name, BooleanSupplier activator) {
+        return new Action(name, activator.getAsBoolean());
+    }
+
+    public static <T> Action allow(String name, T instance, Predicate<T> activator) {
+        return new Action(name, activator.test(instance));
+    }
+
+    public static Action forbid(String name) {
+        return new Action(name, false);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Set<Constraint> getConstraints() {
+        return constraints;
+    }
+
+    public boolean isAllowed() {
+        return allowed;
+    }
+}
---
+++ src/main/java/com/labbati/cando/model/Constraint.java
@@ -0,0 +1,21 @@
+package com.labbati.cando.model;
+
+public class Constraint {
+
+    private String name;
+
+    private Object value;
+
+    public Constraint(String name, Object value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+}
